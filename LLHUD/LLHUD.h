95b615cd639cca6642ce48450abfd21f16a2fc04
ocarol <yulili2020@qq.com>
2015-12-15 23:16:26 +0800

    空壳

---
+++ LLHUD/LLHUD.h
@@ -0,0 +1,13 @@
+//
+//  LLHUD.h
+//  LLHUD
+//
+//  Created by 余丽丽 on 15/12/15.
+//  Copyright © 2015年 ocarol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLHUD : UIView
+
+@end
---
+++ LLHUD/LLHUD.m
@@ -0,0 +1,21 @@
+//
+//  LLHUD.m
+//  LLHUD
+//
+//  Created by 余丽丽 on 15/12/15.
+//  Copyright © 2015年 ocarol. All rights reserved.
+//
+
+#import "LLHUD.h"
+
+@implementation LLHUD
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

4a363fff8b19891d94920bae160b99939954b9b2
ocarol <yulili2020@qq.com>
2015-12-15 23:01:21 +0800

    新项目

---
+++ LLHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLHUD
+//
+//  Created by 余丽丽 on 15/12/15.
+//  Copyright © 2015年 ocarol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLHUD
+//
+//  Created by 余丽丽 on 15/12/15.
+//  Copyright © 2015年 ocarol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

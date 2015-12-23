5b4bd485919cb54dfb29b3446ef705753a63ef5b
ocarol <yulili2020@qq.com>
2015-12-23 22:30:33 +0800

    排版

--- LLHUD/UIView+Frame.m
+++ LLHUD/UIView+Frame.m
@@ -16,26 +16,31 @@
     frame.size = size;
     self.frame = frame;
 }
+
 - (CGSize)size
 {
     return self.frame.size;
 }
+
 - (void)setX:(CGFloat)x
 {
     CGRect frame = self.frame;
     frame.origin.x = x;
     self.frame = frame;
 }
+
 - (CGFloat)x
 {
     return self.frame.origin.x;
 }
+
 - (void)setY:(CGFloat)y
 {
     CGRect frame = self.frame;
     frame.origin.y = y;
     self.frame = frame;
 }
+
 - (CGFloat)y
 {
     return self.frame.origin.y;
